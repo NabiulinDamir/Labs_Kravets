a0804fa15319f36cd98e72b4dd8879c80c4bcd04
NabiulinDamir <wirelles2015@gmail.com>
2025-01-30 20:36:09 +0500

    fix

--- vue-project/src/components/Todoo.vue
+++ vue-project/src/components/Todoo.vue
@@ -27,7 +27,7 @@
 </template>
 
 <script setup>
-import { useCurrentNoteStore } from '@/Notes/store/note';//..f..
+import { useCurrentNoteStore } from '@/Notes/store/note';//..f..kjnkjn
 import { ref } from 'vue';
 const NotesStore = useCurrentNoteStore()
 

c505d7f0b71d7fa09fbe07f1ff632f4003dba090
NabiulinDamir <wirelles2015@gmail.com>
2025-02-04 22:05:39 +0500

    8

--- vue-project/vitepress/.vitepress/config.js
+++ vue-project/vitepress/.vitepress/config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/Labs_Kravets/',
   title: "My Awesome Project",
   description: "A VitePress Site",
   themeConfig: {

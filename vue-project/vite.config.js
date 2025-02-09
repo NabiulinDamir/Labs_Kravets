b620190688427ccc3bc0e5044ba109075d8a5e67
NabiulinDamir <wirelles2015@gmail.com>
2025-02-09 17:26:19 +0500

    reset config base

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     VueDevTools(),
   ],
-  base: "Labs_Kravets",
+  base: process.env.NODE_ENV === 'production' ? '/Labs_Kravets/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

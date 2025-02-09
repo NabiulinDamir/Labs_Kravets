44ffeb91c12e6b7ebe7866978054668803d7fca5
NabiulinDamir <wirelles2015@gmail.com>
2025-02-09 17:24:15 +0500

    set config base

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     VueDevTools(),
   ],
+  base: "Labs_Kravets",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

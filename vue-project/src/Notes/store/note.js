28102127e3c039f3a4aaf8c2448d74a726a6d81b
NabiulinDamir <wirelles2015@gmail.com>
2025-02-12 18:08:15 +0500

    fix jsdoc

--- vue-project/src/Notes/store/note.js
+++ vue-project/src/Notes/store/note.js
@@ -1,5 +1,10 @@
+/**
+ * @module store
+ * @description Vuex-стор для работы с компонентом src/components/Todoo.vue
+ */
 import { defineStore } from 'pinia'
 import { getAll, apiSetNote } from '../api'
+
 /**
  * Класс для работы с заметками.
  */
@@ -13,36 +18,40 @@ export const useCurrentNoteStore = defineStore('CurrentNote', {
   },
 
   actions: {
-  /**
-   * Устанавливает список заметок.
-   * @param {Array} this.notes - Список заметок
-   */
-    setNotes(){
-      this.notes = getAll()
+    /**
+     * Устанавливает список заметок.
+     * @param {Array} notes - Список заметок.
+     */
+    setNotes() {
+      this.notes = getAll();
     },
-  /**
-   * Получчает список заметок.
-   * @param {Array} this.notes - Список заметок
-   */
-    getNotes(){
-        if (this.notes === null) {this.notes = getAll()}
-        return this.notes
+
+    /**
+     * Получает список заметок.
+     * @returns {Array} Список заметок.
+     */
+    getNotes() {
+      if (this.notes === null) {
+        this.notes = getAll();
+      }
+      return this.notes;
     },
-      /**
-   * Добавляет новую заметку.
-   * @returns {int} Id новой заметки.
-   */
-    addNote(){
+
+    /**
+     * Добавляет новую заметку.
+     * @returns {number} Id новой заметки.
+     */
+    addNote() {
       const Id = (this.notes.length > 0 ? Math.max(...this.notes.map(note => (typeof note.id === 'number' ? note.id : -Infinity))) : 0) + 1;
       const currentTime = new Date().toISOString();
-      this.notes.push({"id": Id, "created_at" : currentTime})
-      return Id
-      
+      this.notes.push({ "id": Id, "created_at": currentTime });
+      return Id;
     },
-   /**
-   * Изменяет заметку.
-   * @param {object} item получает измененную заметку.
-   */
+
+    /**
+     * Изменяет заметку.
+     * @param {object} item - Получает измененную заметку.
+     */
     setNote(item) {
       const index = this.notes.findIndex(note => note.id === item.id);
       if (index !== -1) {
@@ -50,17 +59,17 @@ export const useCurrentNoteStore = defineStore('CurrentNote', {
         apiSetNote(item); 
       }
     },
+
     /**
-   * Удаляет заметку.
-   * @param {int} item получает id заметки, которую нужно удалить.
-   */
+     * Удаляет заметку.
+     * @param {number} id - Получает id заметки, которую нужно удалить.
+     */
     deleteNote(id) {
       const index = this.notes.findIndex(note => note.id === id);
       if (index !== -1) {
         this.notes.splice(index, 1); // Удаляем элемент из массива
       }
     }
-    
   },
   persist: true
-})
+});

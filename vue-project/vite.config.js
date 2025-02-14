b238a2789012189dba753e1d179b9942f41cda85
NabiulinDamir <wirelles2015@gmail.com>
2025-02-14 17:06:53 +0500

    точно финал

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     VueDevTools(),
   ],
-  // base: process.env.NODE_ENV === 'production' ? '/Labs_Kravets/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Labs_Kravets/' : '/',
   base: './',
   resolve: {
     alias: {
